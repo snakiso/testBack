34e79f85b43d481d911808015db3c1e50c87aaf5
Ваня <snakiso1@gmail.com>
2024-10-19 17:11:16 +0300

    bfg

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import {app} from "./src/app.js";
 const port = process.env.PORT || 3000
 
 
-    app.listen(port, () => {
+    app.listen(4444, (e) => {
+        if (e) {
+            return console.log(e)
+        }
         console.log(`Example app listening on port ${port}`)
     })
