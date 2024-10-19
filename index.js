880fd4ddf821818404dcccebc8a71a1f104f8f9f
Ваня <snakiso1@gmail.com>
2024-10-19 17:09:33 +0300

    bfg

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ import {app} from "./src/app.js";
 const port = process.env.PORT || 3000
 
 
-const startApp = async () => {
-    try{
-        console.log('res')
-    }catch (e){
-        console.log(e)
-    }
-
-
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
-}
-startApp()
