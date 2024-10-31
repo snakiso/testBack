e294f8a089f75c2d30a1df7cbd15b868b26a5d3c
Ваня <snakiso1@gmail.com>
2024-10-31 15:38:10 +0300

    bfg

--- src/routes/promoCode.js
+++ src/routes/promoCode.js
@@ -5,15 +5,15 @@ export const PromoCodeInfo = Router()
 
 PromoCodeInfo.post('/',  (req, res) => {
     const {promo_code} = req.body
-    if(promo_code) {
-        let resuslt = promoCode[promo_code]
-       if(!resuslt){
-           return res.status(404).json({message: 'promo code not found'})
-       } else{
-           return res.status(200).json(resuslt)
-       }
-    }
-
+    // if(promo_code) {
+    //     let resuslt = promoCode[promo_code]
+    //    if(!resuslt){
+    //        return res.status(404).json({message: 'promo code not found'})
+    //    } else{
+    //        return res.status(200).json(resuslt)
+    //    }
+    // }
+    return res.status(200).json('hello world')
     }
 )
 
