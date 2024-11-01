2bcf9b2c6ed96d44e8de6340a3f551af13c6659b
Ваня <snakiso1@gmail.com>
2024-11-01 18:03:02 +0300

    bfg

--- src/routes/Ticket.js
+++ src/routes/Ticket.js
@@ -3,7 +3,15 @@ import {freePlaces} from "../data/freePlaces.js";
 
 export const Ticket = Router()
 
-Ticket.post('/',  (req, res) => {
-        return res.status(200).json(freePlaces)
-    }
-)
+Ticket.get('/', (req, res) => {
+    const { parking_ticket, personal_data_usage_accepted, reservation_terms_accepted, lang } = req.query;
+
+    // You can use the query parameters as needed
+    console.log("Parking Ticket:", parking_ticket);
+    console.log("Personal Data Usage Accepted:", personal_data_usage_accepted);
+    console.log("Reservation Terms Accepted:", reservation_terms_accepted);
+    console.log("Language:", lang);
+
+    // Return the free places data
+    return res.status(200).json(freePlaces);
+});
