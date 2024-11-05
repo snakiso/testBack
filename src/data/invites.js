293ca1cd665b83d9e1198d88b4584ab693b28a91
Ваня <snakiso1@gmail.com>
2024-11-05 10:14:05 +0300

    bfg

--- src/data/invites.js
+++ src/data/invites.js
@@ -1,8 +1,8 @@
 export const invite = {
-    "type": "in",
-    "user": {
-        "phone": "+79999896300",
-        "public_name": "Иван"
-    },
-    "invite_id": 1
+    // "type": "in",
+    // "user": {
+    //     "phone": "+79999896300",
+    //     "public_name": "Иван"
+    // },
+    // "invite_id": 1
 }
